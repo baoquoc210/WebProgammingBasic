9fd481721f9c230f3f6f65da09a1845f08eb2c26
Huynh Quoc Bao <12quocbao@gmail.com>
2024-11-29 19:53:38 +0700

    Delete TEST.java

--- TEST.java
+++
@@ -1,7 +0,0 @@
-public class TEST {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("GittHUBBB"); 
-        System.out.println("hehehhe");
-    }
-}
